ea30ba51654c767869d10d10e69a8884f6df2aa0
surya2706 <karthiksurya.official@gmail.com>
2023-01-30 17:02:06 +0530

    refactor

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import CartContainer from "../components/CartContainer.vue";
-import CartSummary from "../components/CartSummary.vue";
+import CartContainer from '../components/CartContainer.vue';
+import CartSummary from '../components/CartSummary.vue';
 </script>
 <template>
   <main>
-    <!-- <p>Cart</p> -->
     <div :style="{ display: 'flex' }">
       <CartContainer />
       <CartSummary />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import ShoppingCartHome from "../components/ShoppingCartHome.vue";
+import FilterProducts from '@/components/FilterProducts.vue';
+import ProductList from '@/components/ProductList.vue';
 </script>
 
 <template>
   <main>
-    <ShoppingCartHome />
+    <span class="home">
+      <FilterProducts />
+      <ProductList />
+    </span>
   </main>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: row;
+}
+</style>
